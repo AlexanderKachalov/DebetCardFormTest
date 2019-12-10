7781fbdaf24db443ab6f46704b3ce6ca5d044505
Alexander Kachalov <kachalow77@gmail.com>
2019-12-11 01:05:23 +0300

    updated TestClass.java added

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,5 +1,4 @@
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Selenide;
+
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -8,44 +7,35 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestClass {
+    SelenideElement form;
+
     @Test
     void testFormIfValidateAllInputData() {
-        open("http://localhost:9999");
-        SelenideElement form;
-        form = $(By.className("App_appContainer__3jRx1"));
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79035783484");
-        form.$("[data-test-id=agreement]").click();
-        form.$(By.className("button")).click();
+        inputData("Василий", "+79035783484");
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена!" +
                 " Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void testFormIfNotValidateName() {
-        open("http://localhost:9999");
-        SelenideElement form;
-        form = $(By.className("App_appContainer__3jRx1"));
-        ElementsCollection elements = Selenide.elements(By.className("input__sub"));
-        form.$("[data-test-id=name] input").setValue("Vasiliy");
-        form.$("[data-test-id=phone] input").setValue("+79035783484");
-        form.$("[data-test-id=agreement]").click();
-        form.$(By.className("button")).click();
-        $(elements.get(0)).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские" +
+        inputData("Vasiliy", "+79035783484");
+        form.$$(".input__sub").first().shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские" +
                 " буквы, пробелы и дефисы."));
     }
 
     @Test
     void testFormIfNotValidatePhone() {
-        open("http://localhost:9999");
-        SelenideElement form;
-        form = $(By.className("App_appContainer__3jRx1"));
-        ElementsCollection elements = Selenide.elements(By.className("input__sub"));
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+7(903)578-34-84");
-        form.$("[data-test-id=agreement]").click();
-        form.$(By.className("button")).click();
-        $(elements.get(1)).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например," +
+        inputData("Василий", "+7(903)578-34-84");
+        form.$$(".input__sub").last().shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например," +
                 " +79012345678."));
+    }
+
+    private void inputData (String text, String tel) {
+        open("http://localhost:9999");
+        form = $(By.className("App_appContainer__3jRx1"));
+        form.$("[data-test-id=name] input").setValue(text);
+        form.$("[data-test-id=phone] input").setValue(tel);
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
     }
 }
